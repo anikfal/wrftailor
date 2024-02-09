f5a47782d6b58e44060998424baff3e0a240ce7e
Amir H. Nikfal <ah.nikfal@gmail.com>
2024-02-09 23:45:48 +0100

    update whole domain

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# sed -n '/Variable/s/.*=//p' namelist.modify | tr -d " "
-
 awk_read_onoff() {
     awk -v pat=$1 '$0~pat {print $3}' namelist.modify
 }
@@ -13,16 +11,27 @@ wholeonoff=$(awk_read_onoff whole_domain_ON_OFF)
 
 sumopts=$((shapeonoff + boundonoff + pointsonoff + wholeonoff))
 if [[ $sumopts -gt 1 ]]; then
-    echo ""
-    echo "  More than one task is enabled"
+    echo "  Warning: more than one task is enabled"
     echo "  Select only one task in namelist.modify and run again"
-    echo ""
+    exit
 fi
 if [[ $sumopts -eq 0 ]]; then
-    echo ""
-    echo "  No section is activated"
+    echo "  Warning: no section is activated"
     echo "  Select one task or section in namelist.wrf and run again"
-    echo ""
+    exit
+fi
+
+export wrf_variable=`sed -n "/wrf_variable/s/.*=//p" namelist.modify | tr -d " "`
+export new_value=`sed -n "/new_value/s/.*=//p" namelist.modify | tr -d " "`
+export number_of_domains=`sed -n "/number_of_domains/s/.*=//p" namelist.modify | tr -d " "`
+export domain_1=`sed -n "/domain_1/s/.*=//p" namelist.modify | tr -d " "`
+export domain_2=`sed -n "/domain_2/s/.*=//p" namelist.modify | tr -d " "`
+export domain_3=`sed -n "/domain_3/s/.*=//p" namelist.modify | tr -d " "`
+export domain_4=`sed -n "/domain_4/s/.*=//p" namelist.modify | tr -d " "`
+export domain_5=`sed -n "/domain_5/s/.*=//p" namelist.modify | tr -d " "`
+
+if [[ $wholeonoff == 1 ]]; then
+    ncl -Qn a.ncl
 fi
 
 if [[ $shapeonoff == 1 ]]; then
@@ -30,8 +39,6 @@ if [[ $shapeonoff == 1 ]]; then
     export shape_path=`sed -n "/$myvar/s/.*=//p" namelist.modify | tr -d " "`
     unset myvar
 fi
-
-echo $shape_path
 
 domain_1=/home/anikfal/extra_codes/SR/emissions/wrfchemi/wrfchemi_d01_2021-01-22_00:00:00
 domain_2=/home/anikfal/extra_codes/SR/emissions/wrfchemi/wrfchemi_d02_2021-01-22_00:00:00
